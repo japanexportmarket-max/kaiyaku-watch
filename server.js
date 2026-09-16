d4fd545bc63bc5b2a7fb9534c558fde38103f3f2
japanexportmarket-max <japan.export.market@gmail.com>
2026-09-16 16:42:11 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+app.use(function (_req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  next();
+});
+app.options("*", (_req, res) => res.sendStatus(204));
 
 const PORT = process.env.PORT || 3000;
 const LINE_TOKEN = process.env.LINE_TOKEN || "";
